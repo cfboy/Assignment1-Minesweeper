a1b42d2c5744a2bbd46c28738f412422f789e1f2
Gladymar O'Neill <Gladymar O'Neill@Gladymar-PC>
2016-10-08 12:13:30 -0400

    Methods and variables to create ten random mines in the grid

--- P1/src/MyPanel.java
+++ P1/src/MyPanel.java
@@ -6,18 +6,28 @@ import java.util.Random;
 import javax.swing.JPanel;
 
 public class MyPanel extends JPanel {
+	private Random generator = new Random();
+	//Decalracion de variables estaticas
 	private static final long serialVersionUID = 3426940946811133635L;
 	private static final int GRID_X = 25;
 	private static final int GRID_Y = 25;
 	private static final int INNER_CELL_SIZE = 29;
 	private static final int TOTAL_COLUMNS = 9;
 	private static final int TOTAL_ROWS = 9;   //Last row has only one cell
+	private static final int TOTAL_MINES =10; 
+	
 	public int x = -1;
 	public int y = -1;
 	public int mouseDownGridX = 0;
 	public int mouseDownGridY = 0;
-	public Color[][] colorArray = new Color[TOTAL_COLUMNS][TOTAL_ROWS];
+	public Color[][] colorArray = new Color[TOTAL_COLUMNS][TOTAL_ROWS]; //array de dos dimensiones
+	public String minesAtTime; 
+	public int mines = 0;
+	
+	
 	public MyPanel() {   //This is the constructor... this code runs first to initialize
+		
+		//exceptions si los parametros de tamaño no se cumplen
 		if (INNER_CELL_SIZE + (new Random()).nextInt(1) < 1) {	//Use of "random" to prevent unwanted Eclipse warning
 			throw new RuntimeException("INNER_CELL_SIZE must be positive!");
 		}
@@ -27,18 +37,18 @@ public class MyPanel extends JPanel {
 		if (TOTAL_ROWS + (new Random()).nextInt(1) < 3) {	//Use of "random" to prevent unwanted Eclipse warning
 			throw new RuntimeException("TOTAL_ROWS must be at least 3!");
 		}
-		for (int x = 0; x < TOTAL_COLUMNS; x++) {   //Top row
-			colorArray[x][0] = Color.WHITE;
-		}
-		for (int y = 0; y < TOTAL_ROWS; y++) {   //Left column
-			colorArray[0][y] = Color.WHITE;
-		}
-		for (int x = 1; x < TOTAL_COLUMNS; x++) {   //The rest of the grid
-			for (int y = 1; y < TOTAL_ROWS; y++) {
+		
+		
+		//doble loop para pintar todos los cuadros color blanco
+		for (int x = 0; x < TOTAL_COLUMNS; x++) {   //The rest of the grid
+			for (int y = 0; y < TOTAL_ROWS; y++) {
 				colorArray[x][y] = Color.WHITE;
 			}
 		}
+	
+		minesGenerator();
 	}
+	
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 
@@ -52,23 +62,27 @@ public class MyPanel extends JPanel {
 		int height = y2 - y1;
 
 		//Paint the background
-		g.setColor(Color.LIGHT_GRAY);
+		g.setColor(new Color(0xF5F5DC));
 		g.fillRect(x1, y1, width + 1, height + 1);
 
 		//Draw the grid minus the bottom row (which has only one cell)
 		//By default, the grid will be 10x10 (see above: TOTAL_COLUMNS and TOTAL_ROWS) 
 		g.setColor(Color.BLACK);
-		for (int y = 0; y <= TOTAL_ROWS; y++) {
+		//dibujar las lineas que dividen cada recuadro del grid 10x10
+		for (int y = 0; y <= TOTAL_ROWS ; y++) {
 			g.drawLine(x1 + GRID_X, y1 + GRID_Y + (y * (INNER_CELL_SIZE + 1)), x1 + GRID_X + ((INNER_CELL_SIZE + 1) * TOTAL_COLUMNS), y1 + GRID_Y + (y * (INNER_CELL_SIZE + 1)));
-		}
+		}//lineas horizontales
 		for (int x = 0; x <= TOTAL_COLUMNS; x++) {
-			g.drawLine(x1 + GRID_X + (x * (INNER_CELL_SIZE + 1)), y1 + GRID_Y, x1 + GRID_X + (x * (INNER_CELL_SIZE + 1)), y1 + GRID_Y + ((INNER_CELL_SIZE + 1) * (TOTAL_ROWS)));
-		}
-
+			g.drawLine(x1 + GRID_X + (x * (INNER_CELL_SIZE + 1)), y1 + GRID_Y , x1 + GRID_X + (x * (INNER_CELL_SIZE + 1)), y1 + GRID_Y + ((INNER_CELL_SIZE + 1) * (TOTAL_ROWS )));
+		}//lineas verticales
+		
+		minesAtTime = "" + TOTAL_MINES;
+		g.drawString("Remaining mines:"+minesAtTime, x1+200, y1+20);
+		
 		//Paint cell colors
 		for (int x = 0; x < TOTAL_COLUMNS; x++) {
 			for (int y = 0; y < TOTAL_ROWS; y++) {
-				if ((x == 0) || (y != TOTAL_ROWS)) {
+				if ((x == 0) || (y != TOTAL_ROWS )) {
 					Color c = colorArray[x][y];
 					g.setColor(c);
 					g.fillRect(x1 + GRID_X + (x * (INNER_CELL_SIZE + 1)) + 1, y1 + GRID_Y + (y * (INNER_CELL_SIZE + 1)) + 1, INNER_CELL_SIZE, INNER_CELL_SIZE);
@@ -76,6 +90,44 @@ public class MyPanel extends JPanel {
 			}
 		}
 	}
+	
+
+	/*verifies that there are ten mines and the method 
+	 * randomMines() did not repeat the same square in 
+	 * the grid*/
+	public void minesGenerator(){
+		tenMines();
+		do{
+		for (int i=0; i<TOTAL_COLUMNS; i++){
+			for (int j=0; j<TOTAL_ROWS; j++){
+				if(colorArray[i][j]==Color.BLACK){
+					mines++;
+				}
+			}
+		}
+		if(mines!= TOTAL_MINES){
+			randomMines();
+		}
+		}while(mines!=TOTAL_MINES);
+	}
+	
+	//colors a random square in the grid black, equivalent to a mine
+	public void randomMines(){
+		Random r = new Random();
+		int x = r.nextInt(TOTAL_COLUMNS);
+		int y = r.nextInt(TOTAL_ROWS);
+		colorArray[x][y] = Color.black;
+		//repaint();
+	}
+				
+	//places 10 random mines 
+	public void tenMines(){
+		for(int i = 0; i<10; i++){
+			randomMines();
+		}
+	}
+
+	
 	public int getGridX(int x, int y) {
 		Insets myInsets = getInsets();
 		int x1 = myInsets.left;
@@ -96,11 +148,12 @@ public class MyPanel extends JPanel {
 		if (x == 0 && y == TOTAL_ROWS ) {    //The lower left extra cell
 			return x;
 		}
-		if (x < 0 || x > TOTAL_COLUMNS || y < 0 || y > TOTAL_ROWS) {   //Outside the rest of the grid
+		if (x < 0 || x > TOTAL_COLUMNS  || y < 0 || y > TOTAL_ROWS ) {   //Outside the rest of the grid
 			return -1;
 		}
 		return x;
 	}
+	
 	public int getGridY(int x, int y) {
 		Insets myInsets = getInsets();
 		int x1 = myInsets.left;
@@ -121,10 +174,9 @@ public class MyPanel extends JPanel {
 		if (x == 0 && y == TOTAL_ROWS ) {    //The lower left extra cell
 			return y;
 		}
-		if (x < 0 || x > TOTAL_COLUMNS || y < 0 || y > TOTAL_ROWS) {   //Outside the rest of the grid
+		if (x < 0 || x > TOTAL_COLUMNS  || y < 0 || y > TOTAL_ROWS ) {   //Outside the rest of the grid
 			return -1;
 		}
 		return y;
 	}
-	
 }
